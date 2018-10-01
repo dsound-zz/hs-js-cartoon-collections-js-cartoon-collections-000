a27c058cbc1bd91beb698e6ce96bf11f367f4974
demian sims <demiansims@gmail.com>
2018-10-01 00:19:48 +0000

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ function dwarfRollCall(dwarves) {
   var addDwarves = [];
 
  for (let i=0; i < dwarves.length; i++) {
+
    addDwarves.push(`${i + 1}. ${dwarves[i]} `)
 } 
- return addDwarves.toString().toJoin("") }
+ return addDwarves.join("") 
+  
+} 
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+ var upperCalls = [];
+for (let i=0; i < planeteerCalls.length; i++) {
+  upperCalls.push(planteerCalls[i] + "! ").toUpperCase();
+} return upperCalls;
+
 }
 
 function longPlaneteerCalls(words) {
