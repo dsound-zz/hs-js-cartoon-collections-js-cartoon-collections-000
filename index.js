6fb23b4ec59a52ded5f7f9839c826d239870bf8c
demian sims <demiansims@gmail.com>
2018-10-01 10:17:29 +0000

    Done.

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ return callsArray
 
 
 function longPlaneteerCalls(words) {
+} for (let i=0; i < words.length; i++) {
+  if (words[i].length > 4) { return true 
+}
+else {
+    return false
+  }
 }
 
 function findTheCheese (foods) {
