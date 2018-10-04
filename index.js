04aa7e375acf64dea8745ea544b80fde46937d8b
demian sims <demiansims@gmail.com>
2018-10-04 20:44:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
   var numDwarves = [];
- for (let i=0; i < dwarves.length; i++) {
+ for (let i=dwarves.length/2; i < dwarves.length; i++) {
   numDwarves.push(`${i+1}. ${dwarves[i]} `)
 }
 return numDwarves.join(""); 
